43f4db2c1a799dd9748cf72a3dde61ae29986e80
atyrin <tyrin.andrew@gmail.com>
2019-11-05 22:37:42 +0100

    first commit

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+const Telegraf = require('telegraf')
+
+const bot = new Telegraf("1012104538:AAGSWMzzqEtOQrE1o46_F0zMnTZ7KnQ7nTs")//process.env.BOT_TOKEN)
+bot.start((ctx) => ctx.reply('Welcome'))
+bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.on('sticker', (ctx) => ctx.reply('👍'))
+bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+bot.launch()
