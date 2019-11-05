93806ec8ca57d451709b9da401ccdbdf6295b3f2
atyrin <tyrin.andrew@gmail.com>
2019-11-05 22:47:08 +0100

    add start script

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 const Telegraf = require('telegraf')
 
+console.log("Register Bot")
 const bot = new Telegraf("1012104538:AAGSWMzzqEtOQrE1o46_F0zMnTZ7KnQ7nTs")//process.env.BOT_TOKEN)
 bot.start((ctx) => ctx.reply('Welcome'))
 bot.help((ctx) => ctx.reply('Send me a sticker'))
