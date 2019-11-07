bfa786034e122230e5839a48319e9e86495ae8d4
atyrin <tyrin.andrew@gmail.com>
2019-11-07 22:29:23 +0100

    add tests for parcer

---
+++ readme.md
@@ -0,0 +1,11 @@
+# Installation
+1. clone source code
+2. download latest NODE.JS framework
+3. install dependencies: `npm install`
+4. start bot with: `npm start "bot_token" "zomato_token"`
+  Zomato token you can get here: https://developers.zomato.com/api  
+  Bot token is producing by Telegram Bot Father.
+
+
+# Test Run
+`npm test`
--- src/Restaurants/Kolkovna.ts
+++ src/Restaurants/Kolkovna.ts
@@ -13,28 +13,43 @@ export class Kolkovna implements Restaurant {
         return await fetch(this.URL)
             .then((response: Response) => response.text())
             .then(text => {
-                const root: HTMLElement = HTMLParser.parse(text);
-                let date: any = root.querySelector('div.dailyMenuWeek h2.brown').childNodes[0];
-                let table = root.querySelector('div.dailyMenuWeek table.menu.dailyMenu').childNodes;
-
-                let dishes: Array<Dish> = [];
-                for (const item of table) {
-                    if (item.nodeType !== 1) continue; //not htmlelement
-                    let h = item as HTMLElement;
-
-                    let dname: any = h.querySelector('.name').childNodes[0];
-                    let dprice: any = h.querySelector('.price').childNodes[0];
-                    dishes.push(new Dish({
-                        name: dname.rawText,
-                        translatedname: dname.rawText,
-                        price: dprice.rawText
-                    }))
-                }
-                return new Menu({
-                    date: date.rawText,
-                    dishes: dishes
-                });
+                return this.parse(text);
             });
+    }
+
+    async parse(text): Promise<Menu> {
+        const root: HTMLElement = HTMLParser.parse(text);
+        let date: string = await this.ejectDate(root);
+        let dishes: Array<Dish> = await this.ejectDishes(root);
+
+        return new Menu({
+            date: date,
+            dishes: dishes
+        });
+    }
+
+    private async ejectDate(block: HTMLElement): Promise<string> {
+        let date: any = block.querySelector('div.dailyMenuWeek h2.brown').childNodes[0];
+        return date.rawText;
+    }
+
+    private async ejectDishes(block: HTMLElement): Promise<Array<Dish>> {
+        let table = block.querySelector('div.dailyMenuWeek table.menu.dailyMenu').childNodes;
+
+        let dishes: Array<Dish> = [];
+        for (const item of table) {
+            if (item.nodeType !== 1) continue; //not htmlelement
+            let h = item as HTMLElement;
+
+            let dname: any = h.querySelector('.name').childNodes[0];
+            let dprice: any = h.querySelector('.price').childNodes[0];
+            dishes.push(new Dish({
+                name: dname.rawText,
+                translatedname: dname.rawText,
+                price: dprice.rawText
+            }))
+        }
+        return dishes;
     }
 
     async getWeekMenu(): Promise<Array<Menu>> {
