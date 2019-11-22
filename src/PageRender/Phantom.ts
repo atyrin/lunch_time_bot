c2c8d0b5def233d5ac5548e248809b3a61af447d
atyrin <tyrin.andrew@gmail.com>
2019-11-22 21:16:46 +0100

    add phantom pool

--- src/PageRender/Phantom.ts
+++ src/PageRender/Phantom.ts
@@ -1,23 +1,29 @@
-const phantom = require('phantom');
+const createPhantomPool = require('phantom-pool')
 
 export default class Phantom {
     private readonly URL: string;
     private instance;
     private page;
 
+    private static pool = Phantom.getPhantomPool()
+
     constructor(url: string) {
         this.URL = url;
     }
 
     open = async () => {
-        this.instance = await phantom.create();
-        this.page = await this.createPhantomPage();
-        const status = await this.page.open(this.URL);
-        console.log(`[phantom] Page loading response status: ${status}`);
+        await Phantom.pool.use(
+            async (instance) => {
+                this.page = await instance.createPage();
+                await this.page.setting('userAgent', "Mozilla/5.0 (Unknown; Linux x86_64) AppleWebKit/538.1 (KHTML, like Gecko) PhantomJS/2.1.1 Safari/538.1");
+                const status = await this.page.open(this.URL);
+                console.log(`[phantom] Page loading response status: ${status}`);
+            }
+        )
     };
 
     close = async () => {
-        if(this.instance) this.instance.exit();
+        if (this.instance) this.instance.exit();
     };
 
     executeSelector = async (selector: string): Promise<any> => {
@@ -26,7 +32,7 @@ export default class Phantom {
             const uiObj: string = await this.page.invokeMethod('evaluate', function (s) {
                 return document.querySelector(s);
             }, selector);
-            console.log(`[phantom] Ejected Object: ${uiObj}`);
+            console.log(`[phantom] Method was executeds`);
             return uiObj;
         }
         finally {
@@ -34,9 +40,21 @@ export default class Phantom {
         }
     };
 
-    private async createPhantomPage() {
-        const page = await this.instance.createPage();
-        await page.setting('userAgent', "Mozilla/5.0 (Unknown; Linux x86_64) AppleWebKit/538.1 (KHTML, like Gecko) PhantomJS/2.1.1 Safari/538.1");
-        return page;
+    public static getPhantomPool() {
+        return createPhantomPool({
+            max: 5, // default
+            min: 1, // default
+            // how long a resource can stay idle in pool before being removed
+            idleTimeoutMillis: 30000, // default.
+            // maximum number of times an individual resource can be reused before being destroyed; set to 0 to disable
+            maxUses: 50, // default
+            // function to validate an instance prior to use; see https://github.com/coopernurse/node-pool#createpool
+            validator: () => Promise.resolve(true), // defaults to always resolving true
+            // validate resource before borrowing; required for `maxUses and `validator`
+            testOnBorrow: true, // default
+            // For all opts, see opts at https://github.com/coopernurse/node-pool#createpool
+            phantomArgs: [['--disk-cache=true'], {
+            }], // arguments passed to phantomjs-node directly, default is `[]`. For all opts, see https://github.com/amir20/phantomjs-node#phantom-object-api
+        })
     }
 }
